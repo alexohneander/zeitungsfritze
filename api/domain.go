b5959a9972563049eafffa6b69e6e2a042f334ad
Alex Wellnitz <moin@wellnitz-alex.de>
2023-04-11 20:01:33 +0200

    Add domain api create function

--- api/domain.go
+++ api/domain.go
@@ -1,7 +1,26 @@
 package api
 
-func CreateDomain() {
+import (
+	"github.com/alexohneander/zeitungsfritze/database"
+	"github.com/alexohneander/zeitungsfritze/helper"
+	"github.com/alexohneander/zeitungsfritze/model"
+	"github.com/gofiber/fiber/v2"
+	"github.com/rs/zerolog/log"
+)
 
+var db, dbErr = database.Connect()
+
+func CreateDomain(c *fiber.Ctx) error {
+	domain := helper.CreateDomain()
+	forward := model.Forward{Domain: domain, Target: "127.0.0.1", Port: "8080"}
+
+	result := db.Create(&forward)
+
+	if result.Error != nil || dbErr != nil {
+		log.Error().Msg("Error creating domain: " + result.Error.Error())
+	}
+
+	return c.JSON(fiber.Map{"domain": domain})
 }
 
 func DeleteDomain() {
